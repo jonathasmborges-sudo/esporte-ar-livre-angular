fb570eaa39b4265890046ef03a030e5da7346c8f
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-20 15:59:32 -0300

    Implementa lsitagem e cadastro de corridas

--- src/app/corrida/corrida-lista-component/corrida-lista-component.ts
+++ src/app/corrida/corrida-lista-component/corrida-lista-component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Corrida } from '../../models/corrida';
+import { CorridaService } from '../../service/corrida-service';
 
 @Component({
-  selector: 'app-corrida-component',
+  selector: 'app-corrida-lista-component',
   imports: [],
   templateUrl: './corrida-lista-component.html',
   styleUrl: './corrida-lista-component.css',
 })
-export class CorridaComponent {}
+export class CorridaListaComponent implements OnInit {
+  listaCorridas = signal<Corrida[]>([]);
+
+  constructor(private corridaService: CorridaService) { }
+
+  ngOnInit(): void {
+    this.listaCorridas.set(this.corridaService.listarCorridas());
+
+  }
+
+  inscrever(id: number | undefined) {
+    console.log('Inscrição na corrida ID:', id)
+  }
+}
--- src/app/service/corrida-service.ts
+++ src/app/service/corrida-service.ts
@@ -12,6 +12,6 @@ export class CorridaService {
     }
 
     listarCorridas(){
-        console.log(this.corridas);
+        return this.corridas;
     }
 }
