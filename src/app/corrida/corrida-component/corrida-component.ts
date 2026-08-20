f45d5ed7e8f91b0a2f69700672d81164c1bf6959
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-20 14:16:24 -0300

    Criado o arquivo para listar as corridas cadastradas

---
+++ src/app/corrida/corrida-component/corrida-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corrida-component',
+  imports: [],
+  templateUrl: './corrida-component.html',
+  styleUrl: './corrida-component.css',
+})
+export class CorridaComponent {}
