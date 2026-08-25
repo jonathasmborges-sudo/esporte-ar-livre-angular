1f27792a6419f270679ddff97e2b34195b20368b
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-25 15:46:36 -0300

    Corrige imports nos arquivos spec de corrida

--- src/app/corrida/corrida-lista-component/corrida-lista-component.spec.ts
+++ src/app/corrida/corrida-lista-component/corrida-lista-component.spec.ts
@@ -1,17 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { CorridaListaComponent } from './corrida-lista-component';
 
-import { CorridaComponent } from './corrida-component';
-
-describe('CorridaComponent', () => {
-  let component: CorridaComponent;
-  let fixture: ComponentFixture<CorridaComponent>;
+describe('CorridaListaComponent', () => {
+  let component: CorridaListaComponent;
+  let fixture: ComponentFixture<CorridaListaComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CorridaComponent],
+      imports: [CorridaListaComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CorridaComponent);
+    fixture = TestBed.createComponent(CorridaListaComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
