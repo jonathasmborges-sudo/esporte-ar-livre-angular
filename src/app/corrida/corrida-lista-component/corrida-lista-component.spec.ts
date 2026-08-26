76b14760bc662ef287bea854121ce80db46bbffc
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-26 15:07:51 -0300

    Testes do CorridaService executados com sucesso

--- src/app/corrida/corrida-lista-component/corrida-lista-component.spec.ts
+++ src/app/corrida/corrida-lista-component/corrida-lista-component.spec.ts
@@ -1,27 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { CorridaListaComponent } from './corrida-lista-component';
+import { TestBed } from '@angular/core/testing';
+import { CorridaService } from '../../service/corrida-service';
+import { Corrida } from '../../models/corrida';
 
-describe('CorridaListaComponent', () => {
-  let component: CorridaListaComponent;
-  let fixture: ComponentFixture<CorridaListaComponent>;
+describe('CorridaService', () => {
+  let service: CorridaService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CorridaListaComponent],
-      providers: [
-        provideHttpClient(),
-        provideHttpClientTesting()
-      ]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(CorridaListaComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CorridaService]
+    });
+    service = TestBed.inject(CorridaService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('deve ser criado com sucesso', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('Deve iniciar com a lista de corridas vazia', () => {
+    const corridas = service.listarCorridas();
+    expect(corridas.length).toBe(0);
+  });
+
+  it('Deve adicionar uma corrida', () => {
+    const novaCorrida: Corrida = {
+      data: '2026-09-10',
+      distancia: '5km',
+      descricao: 'Corrida de Aracaju'
+    };
+
+    service.adicionarCorrida(novaCorrida);
+
+    const corridas = service.listarCorridas();
+    expect(corridas.length).toBe(1);
+    expect(corridas[0].descricao).toBe('Corrida de Aracaju');
+    expect(corridas[0].id).toBe(1);
   });
 });
