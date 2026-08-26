ce573af773ba7d4408a8f5d36f13eae33d8f2439
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-26 13:49:20 -0300

    Ajusta testes do AtletaService usando a MockAPI propria

--- src/app/component/atleta/atleta-lista-component/atleta-lista-component.spec.ts
+++ src/app/component/atleta/atleta-lista-component/atleta-lista-component.spec.ts
@@ -1,13 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
 
 import { AtletaService } from '../../../service/atleta-service';
 import { Atleta } from '../../../models/atleta';
 
 describe('AtletaService', () => {
+
   let service: AtletaService;
   let httpMock: HttpTestingController;
+
+  const urlApi = 'https://6a836243cb486d243403a95a.mockapi.io/atleta';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,74 +25,81 @@ describe('AtletaService', () => {
     httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('Deve calcular a idade corretamente', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('deve calcular a idade corretamente', () => {
     const resultado = service.calcularIdade('1976-05-05');
     expect(resultado).toBe(50);
   });
 
-  it('Deve retornar as pessoas', () => {
-    const atletas: Atleta[] = [
+  // GET
+  it('Deve retornar os atletas', () => {
+    const atletasMock: Atleta[] = [
       {
-        id: 1,
-        nome: "João",
-        cpf: 12345678910,
-        sexo: "M",
-        cep: 49123123,
-        bairro: "Centro",
-        cidade: "Aracaju",
-        uf: "Se",
-        dataNascimento: "2000-02-25",
-        ruaLogradouro: "Rua Sei lá das quantas"
-      },
-      {
-        id: 2,
-        nome: "Maria",
-        cpf: 11122233302,
-        sexo: "F",
-        cep: 49123123,
-        bairro: "Centro",
-        cidade: "Aracaju",
-        uf: "Se",
-        dataNascimento: "2010-02-20",
-        ruaLogradouro: "Rua Sei lá das quantas"
-      }
-    ];
-
-    service.listarAtletas().subscribe(dados => {
-      expect(dados.length).toBe(2);
-      expect(dados[0].nome).toBe('João');
-      expect(dados[1].nome).toBe('Maria');
-    });
-
-    //Post
-    it('deve adicionar uma pessoa', () => {
-      const atleta: Atleta = {
-        "nome": "Maria Flor",
+        "nome": "João",
         "cpf": 12345678910,
         "sexo": "M",
-        "cep": 4912345678910,
+        "cep": 49123123,
         "bairro": "Centro",
         "cidade": "Aracaju",
         "uf": "Se",
         "dataNascimento": "2000-02-25",
-        "id": 3,
+        "id": 1,
+        "ruaLogradouro": "Rua Sei lá das quantas"
+      },
+      {
+        "nome": "Maria",
+        "cpf": 11122233302,
+        "sexo": "F",
+        "cep": 49123123,
+        "bairro": "Centro",
+        "cidade": "Aracaju",
+        "uf": "Se",
+        "dataNascimento": "2010-02-20",
+        "id": 2,
         "ruaLogradouro": "Rua Sei lá das quantas"
       }
+    ];
 
-      service.salvarAtleta(atleta).subscribe(atleta => {
-        expect(atletas).toEqual(atletas)
-      })
-    })
+    service.listarAtletas().subscribe(atletas => {
+      expect(atletas.length).toBe(2);
+      expect(atletas[0].nome).toBe('João');
+      expect(atletas[1].nome).toBe('Maria');
+    });
 
-    //const request = httpMock.expectOne('http://localhost:3000/atletas')
-    const request = httpMock.expectOne(`https://6a836243cb486d243403a95a.mockapi.io/atleta/1`)
-
+    const request = httpMock.expectOne(urlApi);
     expect(request.request.method).toBe('GET');
-
-    request.flush(atletas);
+    request.flush(atletasMock);
   });
 
-  //PUT
+  // POST
+  it('deve adicionar uma pessoa', () => {
+    const atleta: Atleta = {
+      "nome": "Maria Flor",
+      "cpf": 12345678910,
+      "sexo": "F",
+      "cep": 49123123,
+      "bairro": "Centro",
+      "cidade": "Aracaju",
+      "uf": "Se",
+      "dataNascimento": "2000-02-25",
+      "id": 3,
+      "ruaLogradouro": "Rua Sei lá das quantas"
+    };
+
+    service.salvarAtleta(atleta).subscribe(res => {
+      expect(res).toEqual(atleta);
+    });
+
+    const request = httpMock.expectOne(urlApi);
+    expect(request.request.method).toBe('POST');
+    expect(request.request.body).toEqual(atleta);
+    request.flush(atleta);
+  });
+
+  // PUT
   it('deve editar um atleta', () => {
     const atleta: Atleta = {
       "nome": "João Souza",
@@ -102,34 +112,25 @@ describe('AtletaService', () => {
       "dataNascimento": "2000-02-25",
       "id": 1,
       "ruaLogradouro": "Rua Sei lá das quantas"
-    }
+    };
 
-    service.alterarAtleta(atleta).subscribe(atletas => {
-      expect(atletas).toEqual(atleta)
-    })
+    service.alterarAtleta(atleta).subscribe(res => {
+      expect(res).toEqual(atleta);
+    });
 
-    const request = httpMock.expectOne(
-      'https://6a836243cb486d243403a95a.mockapi.io/atleta/1'
-    )
+    const request = httpMock.expectOne(`${urlApi}/1`);
+    expect(request.request.method).toBe('PUT');
+    expect(request.request.body).toEqual(atleta);
+    request.flush(atleta);
+  });
 
-    expect(request.request.method).toBe('PUT')
-
-    expect(request.request.body).toEqual(atleta)
-
-    request.flush(atleta)
-  })
-
-  // Delete
+  // DELETE
   it('deve excluir um atleta', () => {
+    service.excluirAtleta(1).subscribe();
 
-    service.excluirAtleta(1).subscribe()
+    const request = httpMock.expectOne(`${urlApi}/1`);
+    expect(request.request.method).toBe('DELETE');
+    request.flush(null);
+  });
 
-    const request = httpMock.expectOne(
-      'https://6a836243cb486d243403a95a.mockapi.io/atleta/1'
-    )
-
-    expect(request.request.method).toBe('DELETE')
-
-    request.flush(null)
-  })
 });
